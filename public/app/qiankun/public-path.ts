23cdb1b8e97d3b9847248a5e923d08dcb0df5e34
songsz <song.shangzhi3@neolink.com>
2023-07-21 10:38:21 +0800

    fix: publicPath issue in Qiankun

--- public/app/qiankun/public-path.ts
+++ public/app/qiankun/public-path.ts
@@ -2,5 +2,6 @@ import { IS_QIANKUN } from './constants';
 
 declare let __webpack_public_path__: string;
 if (IS_QIANKUN) {
-  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ + 'public/build/';
+  const devSubPath = process.env.NODE_ENV === 'development' ? 'public/build/' : '';
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ + devSubPath;
 }
